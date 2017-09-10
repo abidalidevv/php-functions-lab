6b8877afd7baf5c015b0c9db8a89e387d992677b
abidmmp <abidmmp99@gmail.com>
2017-09-10 10:02:00 +0500

    Fix header injection guard

--- core_logic.php
+++ core_logic.php
@@ -100,3 +100,4 @@
 // Add resource transformation - 2022-06-14 10:00:00
 // Improve file upload validation - 2017-10-02 10:10:00
 // Fix null coalescing issue - 2021-03-02 10:06:00
+// Fix header injection guard - 2017-09-10 10:02:00
