e3da6dc2de40c91d36647445895b23c099ed14c6
abidmmp <abidmmp99@gmail.com>
2018-04-18 10:08:00 +0500

    Improve password hashing

--- core_logic.php
+++ core_logic.php
@@ -144,3 +144,4 @@
 // Fix namespace import - 2023-09-02 10:06:00
 // Improve file upload validation - 2021-03-30 10:02:00
 // Optimize database query - 2024-08-03 10:12:00
+// Improve password hashing - 2018-04-18 10:08:00
