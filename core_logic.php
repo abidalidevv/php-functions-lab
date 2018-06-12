f88d78aa4cf3e7a06a284fcdd1b8c3a29deeb0b2
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:02:00 +0500

    Refactor helper functions

--- core_logic.php
+++ core_logic.php
@@ -2,3 +2,4 @@
 // Add config validation - 2020-07-10 10:00:00
 // Fix session handling - 2022-05-07 10:10:00
 // Fix cookie security flags - 2024-11-11 10:06:00
+// Refactor helper functions - 2018-06-12 10:02:00
