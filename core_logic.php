cb08e9434ca4e7da411aab3eff8014f6acd2355d
abidmmp <abidmmp99@gmail.com>
2019-07-31 10:12:00 +0500

    Add database transactions

--- core_logic.php
+++ core_logic.php
@@ -241,3 +241,4 @@
 // Improve file upload validation - 2017-09-04 10:10:00
 // Refactor event listeners - 2021-07-16 10:06:00
 // Fix type coercion bug - 2018-07-09 10:02:00
+// Add database transactions - 2019-07-31 10:12:00
