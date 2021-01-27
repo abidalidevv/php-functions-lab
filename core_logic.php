5d6275d485915f0c402666f01aa9070ebfbd7569
abidmmp <abidmmp99@gmail.com>
2021-01-27 10:12:00 +0500

    Improve query builder

--- core_logic.php
+++ core_logic.php
@@ -73,3 +73,4 @@
 // Fix route parameter binding - 2020-10-09 10:10:00
 // Fix route parameter binding - 2023-08-27 10:06:00
 // Refactor authentication logic - 2021-09-17 10:02:00
+// Improve query builder - 2021-01-27 10:12:00
