2b25d36bff54a01f5f03888a93f84c90c43b0279
abidmmp <abidmmp99@gmail.com>
2021-01-31 10:06:00 +0500

    Fix cookie security flags

--- core_logic.php
+++ core_logic.php
@@ -120,3 +120,4 @@
 // Fix PHP 8 compatibility - 2024-08-02 10:04:00
 // Refactor controller method - 2020-08-12 10:00:00
 // Add input type checking - 2023-09-04 10:10:00
+// Fix cookie security flags - 2021-01-31 10:06:00
