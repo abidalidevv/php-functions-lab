1aefae8511b523a4209d44198153855f810a2ad5
abidmmp <abidmmp99@gmail.com>
2021-04-05 10:08:00 +0500

    Refactor event listeners

--- core_logic.php
+++ core_logic.php
@@ -256,3 +256,4 @@
 // Fix cookie security flags - 2022-09-23 10:06:00
 // Improve response formatting - 2024-03-24 10:02:00
 // Add UUID generation - 2020-02-13 10:12:00
+// Refactor event listeners - 2021-04-05 10:08:00
