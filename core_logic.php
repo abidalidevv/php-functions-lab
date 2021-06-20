916cec42e7adc2ea40d85e6bc2e864fa496e557d
abidmmp <abidmmp99@gmail.com>
2021-06-20 10:04:00 +0500

    Fix PHP 8 compatibility

--- core_logic.php
+++ core_logic.php
@@ -159,3 +159,4 @@
 // Refactor helper functions - 2020-11-08 10:02:00
 // Fix redirect handling - 2024-11-24 10:12:00
 // Add eloquent relationship - 2022-04-15 10:08:00
+// Fix PHP 8 compatibility - 2021-06-20 10:04:00
