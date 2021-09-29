c60a79d5da076437e51317b9619e658f523ce339
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:12:00 +0500

    Improve form request

--- core_logic.php
+++ core_logic.php
@@ -3,3 +3,4 @@
 // Fix session handling - 2022-05-07 10:10:00
 // Fix cookie security flags - 2024-11-11 10:06:00
 // Refactor helper functions - 2018-06-12 10:02:00
+// Improve form request - 2021-09-29 10:12:00
