929a7c6858396edf2b6fee0d17788de8895d3c1c
abidmmp <abidmmp99@gmail.com>
2021-10-25 10:06:00 +0500

    Add resource transformation

--- core_logic.php
+++ core_logic.php
@@ -218,3 +218,4 @@
 // Add resource transformation - 2024-12-01 10:04:00
 // Refactor helper functions - 2023-07-20 10:00:00
 // Add UUID generation - 2018-04-17 10:10:00
+// Add resource transformation - 2021-10-25 10:06:00
