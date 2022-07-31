78241cd130da10a85a60c2c75e6ee77730302843
abidmmp <abidmmp99@gmail.com>
2022-07-31 10:06:00 +0500

    Fix type coercion bug

--- core_logic.php
+++ core_logic.php
@@ -176,3 +176,4 @@
 // Fix namespace import - 2018-07-12 10:04:00
 // Fix namespace import - 2023-03-23 10:00:00
 // Fix route parameter binding - 2017-06-04 10:10:00
+// Fix type coercion bug - 2022-07-31 10:06:00
