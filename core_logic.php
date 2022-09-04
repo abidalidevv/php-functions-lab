162bed6869b122058a4697e8478cd372798a23a0
abidmmp <abidmmp99@gmail.com>
2022-09-04 10:02:00 +0500

    Add eloquent relationship

--- core_logic.php
+++ core_logic.php
@@ -268,3 +268,4 @@
 // Refactor helper functions - 2021-04-01 10:00:00
 // Optimize array operations - 2020-03-04 10:10:00
 // Refactor service layer - 2017-04-02 10:06:00
+// Add eloquent relationship - 2022-09-04 10:02:00
