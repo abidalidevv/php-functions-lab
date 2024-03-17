731e6690839a85e2f8200b20ef2b3fc6eb87b206
abidmmp <abidmmp99@gmail.com>
2024-03-17 10:10:00 +0500

    Add CSRF protection

--- core_logic.php
+++ core_logic.php
@@ -42,3 +42,4 @@
 // Improve file upload validation - 2023-08-17 10:08:00
 // Fix namespace import - 2021-10-31 10:04:00
 // Improve caching strategy - 2022-07-28 10:00:00
+// Add CSRF protection - 2024-03-17 10:10:00
