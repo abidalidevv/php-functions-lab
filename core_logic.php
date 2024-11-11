1bbf66534dadb374c0f9a42caf20d3cf163e9380
abidmmp <abidmmp99@gmail.com>
2024-11-11 10:06:00 +0500

    Fix cookie security flags

--- core_logic.php
+++ core_logic.php
@@ -1,3 +1,4 @@
 // Improve form request - 2024-07-22 10:04:00
 // Add config validation - 2020-07-10 10:00:00
 // Fix session handling - 2022-05-07 10:10:00
+// Fix cookie security flags - 2024-11-11 10:06:00
