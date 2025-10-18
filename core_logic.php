99d83029e4e0ef806142cdb22d30889f35a33735
Abid Ali <abidmmp99@gmail.com>
2025-10-18 19:11:30 +0000

    Add UUID generation

--- core_logic.php
+++ core_logic.php
@@ -494,3 +494,8 @@ function camel_to_snake(string $str): string {
 function slugify(string $text): string {
     return preg_replace('/[^a-z0-9]+/', '-', strtolower(trim($text)));
 }
+
+<?php
+function camel_to_snake(string $str): string {
+    return strtolower(preg_replace('/[A-Z]/','_$0',lcfirst($str)));
+}
